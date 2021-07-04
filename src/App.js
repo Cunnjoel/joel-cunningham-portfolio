9eed07e36476cf2b5c103a6871dea7f99bc2e074
Joel C <cunningham.joel84@gmail.com>
2021-07-03 22:44:47 -0500

    update for deploy

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
  
 render() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div>
         <Header />
         <Route exact path="/" component={Home} />
